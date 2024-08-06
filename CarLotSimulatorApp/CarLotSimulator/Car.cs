d3b1c3377300b16475efacc21201fcb16cfffee9
logan-tolbert <logo88205+git@gmail.com>
2024-08-05 20:46:33 -0500

    Start exercise

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLotSimulator
+{
+    public class Car
+    {
+        public int lotID { get; set; }
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string EngineNoise {  get; set; }
+        public string HonkNoise {  get; set; }
+        public bool IsDrivable { get; set; }
+
+
+        public void MakeEngineNoise(string EngineNoise)
+        {
+            Console.WriteLine(EngineNoise);
+        }
+
+
+        public void MakeHornNoise(string HornNoise)
+        {
+            Console.WriteLine(HornNoise);
+        }
+}
+       
+}
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CarLotSimulator
 {
@@ -6,6 +7,8 @@ namespace CarLotSimulator
     {
         static void Main(string[] args)
         {
+            var myCarLot = new List<CarLot>();
+
             //TODO
 
             //Create a seperate class file called Car
@@ -15,8 +18,57 @@ namespace CarLotSimulator
 
 
             //Now that the Car class is created we can instanciate 3 new cars
+            var car1 = new Car();
+            var car2 = new Car();
+            var car3 = new Car();
+
             //Set the properties for each of the cars
+            car1.lotID = 1;
+            car1.Year = 2006;
+            car1.Make = "Toyota";
+            car1.Model = "Corolla";
+            car1.EngineNoise = "rum-rum-RUM-RUM-ROOOOOM-ROOOM-RUM-Rum-rum-rum";
+            car1.HonkNoise = "Beep-Beep";
+            car1.IsDrivable = true;
+
+            
+            car2.lotID = 2;
+            car2.Year = 2004;
+            car2.Make = "Chevrolet";
+            car2.Model = "Silverado";
+            car2.EngineNoise = "VVVRUM-VRUM-VRUM-VRUM-VROOOOOM-VROOOM-VRUM-VRum-vrum-vrum";
+            car2.HonkNoise = "HONK-HONK";
+            car2.IsDrivable = true;
+
+            car3.lotID = 3;
+            car3.Year = 2013;
+            car3.Make = "Toyota";
+            car3.Model = "Prius";
+            car3.EngineNoise = "";
+            car3.HonkNoise = "meep-meep";
+            car3.IsDrivable = true;
+
+
             //Call each of the methods for each car
+            Console.WriteLine("Engine test: ");
+            car1.MakeEngineNoise(car1.EngineNoise);
+            Console.WriteLine("Horn test:");
+            car1.MakeHornNoise(car1.HonkNoise);
+            Console.WriteLine(Environment.NewLine);
+
+
+            Console.WriteLine("Engine test: ");
+            car2.MakeEngineNoise(car2.EngineNoise);
+            Console.WriteLine("Horn test:");
+            car2.MakeHornNoise(car2.HonkNoise);
+            Console.WriteLine(Environment.NewLine);
+
+
+            Console.WriteLine("Engine test: ");
+            car3.MakeEngineNoise(car3.EngineNoise);
+            Console.WriteLine("Horn test:");
+            car3.MakeHornNoise(car3.HonkNoise);
+            Console.WriteLine(Environment.NewLine);
 
             //*************BONUS*************//
 
